33c385a042f6a3758edd07966638e05ef04fc3af
schramm42 <schramm42@gmail.com>
2019-11-14 10:28:57 +0100

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "LGButton",
+    products: [
+        .library(name: "LGButton", targets: ["LGButton"])
+    ],
+    targets: [
+        .target(
+            name: "LGButton",
+            path: "."
+        )
+    ]
+)
